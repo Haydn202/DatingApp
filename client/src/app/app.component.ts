44d9dfb1819e7a2ece8553b1ba199c80e162ff8c
Haydn202 <haydnthomson@hotmail.com>
2021-03-05 11:50:00 +1300

    register function added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,10 +12,9 @@ export class AppComponent implements OnInit{
   title = 'The Dating App';
   users: any; //turns off type safty for users property.
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -25,12 +24,4 @@ export class AppComponent implements OnInit{
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers(){
-    //get data from API, subscribe sets response to be users
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    });
-  }
 }
